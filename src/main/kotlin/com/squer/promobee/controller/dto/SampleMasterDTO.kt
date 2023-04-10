7f89750b8e84816cceee3cf80c0b88ff43e2d2ea
satendra yadav <89693080+SatendraYadav96@users.noreply.github.com>
2023-04-10 19:05:14 +0530

    sample mapper chnaged

---
+++ src/main/kotlin/com/squer/promobee/controller/dto/SampleMasterDTO.kt
@@ -0,0 +1,18 @@
+package com.squer.promobee.controller.dto
+
+import java.util.Date
+
+class SampleMasterDTO {
+    var id: String ? = null
+    var name: String ?= null
+    var lmid: String ?= null
+    var description: String ?= null
+    var brandId: String ?= null
+    var packSize: String ?= null
+    var active: String ?= null
+    var brandName: String ?= null
+    var brandCode: String ?= null
+    var isActiveBrand: String ?= null
+    var cap: Int? = null
+
+}
