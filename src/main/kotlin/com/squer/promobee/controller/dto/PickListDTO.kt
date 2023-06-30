2772a981b46155f7e2491bf70f5e015a172b14e9
satendra yadav <89693080+SatendraYadav96@users.noreply.github.com>
2023-06-30 12:37:43 +0530

    updated

--- src/main/kotlin/com/squer/promobee/controller/dto/PickListDTO.kt
+++ src/main/kotlin/com/squer/promobee/controller/dto/PickListDTO.kt
@@ -2,31 +2,31 @@ package com.squer.promobee.controller.dto
 
 class PickListDTO {
 
-//    var  month : Int? = null
-//
-//    var  year : Int? = null
+    var  month : Int? = null
+
+    var  year : Int? = null
 
     var planMonth : String? = null
 
-//    var   inventoryID : String? = null
-//
-//    var  designationID : String? = null
+    var   inventoryID : String? = null
+
+    var  designationID : String? = null
 
     var  designationName : String? = null
 
-//    var  expiryDate : String? = null
-//
-//    var  poNo : String? = null
-//
-//    var  batchNo : String? = null
-//
-//    var  stockQty : Int? = null
+    var  expiryDate : String? = null
+
+    var  poNo : String? = null
+
+    var  batchNo : String? = null
+
+    var  stockQty : Int? = null
 
     var  dispatchedQty : Int? = null
 
-//    var  recipientID : String? = null
-//
-//    var  fFID : String? = null
+    var  recipientID : String? = null
+
+    var  fFID : String? = null
 
     var  recipientName : String? = null
 
@@ -34,15 +34,15 @@ class PickListDTO {
 
     var  recipientCode : String? = null
 
-//    var  teamID : String? = null
+    var  teamID : String? = null
 
     var  teamName : String? = null
 
-//    var  itemID : String? = null
+    var  itemID : String? = null
 
     var  itemName : String? = null
 
-//    var  itemCode : String? = null
+    var  itemCode : String? = null
 
-//    var  planPurpose : String? = null
+    var  planPurpose : String? = null
 }
