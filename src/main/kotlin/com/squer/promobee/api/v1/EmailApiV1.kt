b332453f8ae9c1515099c2c0685113a84fd52b73
satendra yadav <89693080+SatendraYadav96@users.noreply.github.com>
2023-10-20 11:50:46 +0530

    updated

--- src/main/kotlin/com/squer/promobee/api/v1/EmailApiV1.kt
+++ src/main/kotlin/com/squer/promobee/api/v1/EmailApiV1.kt
@@ -4,20 +4,28 @@ import com.squer.promobee.controller.EmailController
 import com.squer.promobee.service.EmailService
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 @RequestMapping("/v1/email")
 @Tag(name = "V1 Email APIs")
 @SecurityRequirement(name="bearer-key")
 @CrossOrigin
+
+
 class EmailApiV1(
-    emailService: EmailService, mailSender: JavaMailSender
+    emailService: EmailService,
+
+    @Autowired
+    mailSender: JavaMailSender
 ): EmailController(
-    emailService = emailService, mailSender = mailSender
+    emailService = emailService,
+    mailSender = mailSender
 ) {
 
 
